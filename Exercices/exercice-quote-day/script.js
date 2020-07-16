c6f10fcec6076100caf1645468eb118a7df141fa
sinarinzi-methode <methode@siriusschool.be>
2020-07-16 11:01:56 +0200

    Exercice-Quotes style
    Quotes style

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,22 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+let getRandomQuotes = () => {
+  let randomNumber = Math.floor(Math.random() * Math.floor(1643));
+  fetch('https://type.fit/api/quotes')
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      let data = response[randomNumber];
+      console.log(data);
+      document.getElementById('quotes').textContent = data.text;
+      if (data.author === null || data.author === undefined) {
+        document.getElementById('author').textContent = "l'auteur est inconnu";
+      } else {
+        document.getElementById('author').textContent = data.author;
+      }
+    });
+};
+getRandomQuotes();
+document.getElementById('btn-alerte').addEventListener('click', () => {
+  getRandomQuotes();
+});
